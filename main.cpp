f375113a49a91d2fcc062a0edb2dd3e51fc19eed
Asis Perez <jose.perez2882@alumnos.udg.mx>
2020-10-06 22:49:13 -0500

    Prueba 01

--- main.cpp
+++ main.cpp
@@ -3,7 +3,31 @@
 using namespace std;
 
 int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+    string cadena;
+    getline(cin, cadena);
 
+    if(cadena == "int"){
+        cout << sizeof(int) << endl;
+    }
+    if(cadena == "char")
+    {
+        cout << sizeof(char) << endl;
+    }
+    if(cadena == "short")
+    {
+        cout << sizeof(short) << endl;
+    }
+    if(cadena == "float")
+    {
+        cout << sizeof(float) << endl;
+    }
+    if(cadena == "double")
+    {
+        cout << sizeof(double) << endl;
+    }
+    if(cadena == "long")
+    {
+        cout << sizeof(long) << endl;
+    }
     return 0;
 }
